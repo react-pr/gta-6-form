c97dcdff999e8669ede36297de0859a94335919d
polinom <kutsenko.pn@ucu.edu.ua>
2024-01-14 15:25:41 +0200

    Not use .env filing | commit

--- front-end/src/userServices/UserService.tsx
+++ front-end/src/userServices/UserService.tsx
@@ -8,12 +8,17 @@ interface IData {
 export const UserService = {
 	async getAll(): Promise<IData[]> {
 		const getRequest: string = process.env.REACT_APP_GET as string
-		const response = await axios.get<IData[]>(getRequest)
+		const response = await axios.get<IData[]>(
+			'https://gta-service.onrender.com/users/users/'
+		)
 		return response.data
 	},
 	async pushOne(data: IData): Promise<number> {
 		const postRequest: string = process.env.REACT_APP_POST as string
-		const response = await axios.post<IData>(postRequest, data)
+		const response = await axios.post<IData>(
+			'https://gta-service.onrender.com/new-user/',
+			data
+		)
 		return response.status
 	},
 }
